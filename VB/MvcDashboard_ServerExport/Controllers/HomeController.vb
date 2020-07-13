fb003214992bebe373e40d579346a152224a0f31
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2020-07-13 15:00:08 +0300

    Fix VB.NET sample version

--- VB/MvcDashboard_ServerExport/Controllers/HomeController.vb
+++ VB/MvcDashboard_ServerExport/Controllers/HomeController.vb
@@ -11,34 +11,35 @@ Namespace MvcDashboard_ServerExport.Controllers
             Return View()
         End Function
 
-        <HttpPost> _
-        Public Function ExportDashboardToPdf(ByVal DashboardID As String, ByVal DashboardState As String) As ActionResult
-            Using stream As New MemoryStream()
+		<HttpPost>
+		Public Function ExportDashboardToPdf(ByVal DashboardID As String, ByVal DashboardState As String) As ActionResult
+			Using stream As New MemoryStream()
+				Dim dbID As String = DashboardID
+				Dim dbState As New DashboardState()
 
-                Dim dashboardID_Renamed As String = DashboardID
+				dbState.LoadFromJson(DashboardState)
+				Dim pdfOptions As New DashboardPdfExportOptions()
+				pdfOptions.ExportFilters = True
+				pdfOptions.DashboardStatePosition = DashboardStateExportPosition.Below
+				Dim dateTimeNow As String = Date.Now.ToString("yyyyMMddHHmmss")
+				Dim serverPath As String = Server.MapPath("~/App_Data/Export")
+				If Not Directory.Exists(serverPath) Then
+					Directory.CreateDirectory(serverPath)
+				End If
+				Dim filePath As String = Path.Combine(serverPath, dbID)
+				Dim uniqueId As String = "_" & dateTimeNow & ".pdf"
+				Dim exporter = New WebDashboardExporter(DashboardConfigurator.Default)
+				exporter.ExportToPdf(dbID, stream, New System.Drawing.Size(1024, 768), dbState, pdfOptions)
+				SaveFile(stream, filePath & uniqueId)
+				Dim result As New ContentResult()
+				result.Content = filePath & uniqueId
+				Return result
+			End Using
+		End Function
 
-                Dim dashboardState_Renamed As New DashboardState()
-                dashboardState_Renamed.LoadFromJson(DashboardState)
-
-                Dim pdfOptions As New DashboardPdfExportOptions()
-                pdfOptions.ExportFilters = True
-                pdfOptions.DashboardStatePosition = DashboardStateExportPosition.Below
-
-                Dim dateTimeNow As String = Date.Now.ToString("yyyyMMddHHmmss")
-                Dim filePath As String = "~/App_Data/Export/" & dashboardID_Renamed & "_" & dateTimeNow & ".pdf"
-                Dim exporter As New WebDashboardExporter(DashboardConfigurator.Default)
-                exporter.ExportToPdf(dashboardID_Renamed, stream, New System.Drawing.Size(1024, 768), dashboardState_Renamed, pdfOptions)
-                SaveFile(stream, filePath)
-
-                Dim result As New ContentResult()
-                result.Content = filePath
-                Return result
-            End Using
-        End Function
-
-        Private Sub SaveFile(ByVal stream As MemoryStream, ByVal path As String)
-            Dim fileStream = System.IO.File.Create(Server.MapPath(path))
-            stream.WriteTo(fileStream)
+		Private Sub SaveFile(ByVal stream As MemoryStream, ByVal path As String)
+			Dim fileStream = System.IO.File.Create(path)
+			stream.WriteTo(fileStream)
             fileStream.Close()
         End Sub
     End Class
